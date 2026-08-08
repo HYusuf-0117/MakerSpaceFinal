4dae8ce5c79869b639a2e0c35f6b68170775ac5d
Owen Cabrera <oencab2@gmail.com>
2026-08-08 18:44:19 -0400

    Created the credit service

--- src/main/java/Strategy/CreditCalculator.java
+++ src/main/java/Strategy/CreditCalculator.java
@@ -1,14 +1,82 @@
-package Strategy;
-
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * User Responsible: Owen Cabrera
+ * Course: CST8288
+ * Maker Space Final Project
+ *
+ * Description:
+ * This class is the Context for the Strategy pattern. It manages the
+ * selection and execution of credit calculation strategies based on
+ * user type.
  */
+
+package strategy;
+
+import DTO.User;
 
 /**
+ * Context class that manages credit calculation strategies.
  *
- * @author oenca
+ * @author Owen Cabrera
+ * @version 1.0
  */
 public class CreditCalculator {
+    private CreditCalculationStrategy strategy;
     
+    /**
+     * Sets the strategy based on user type.
+     *
+     * @param user the user whose strategy should be used
+     */
+    public void setStrategy(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        
+        String userType = user.getUserType();
+        
+        if ("Trainer".equalsIgnoreCase(userType)) {
+            this.strategy = new TrainerCreditStrategy();
+        } else if ("Shop-Tech".equalsIgnoreCase(userType)) {
+            this.strategy = new ShopTechCreditStrategy();
+        } else {
+            this.strategy = new UserCreditStrategy();
+        }
+    }
+    
+    /**
+     * Sets a specific strategy directly.
+     *
+     * @param strategy the strategy to use
+     */
+    public void setStrategy(CreditCalculationStrategy strategy) {
+        this.strategy = strategy;
+    }
+    
+    /**
+     * Calculates credits for a user's activity.
+     *
+     * @param user the user performing the activity
+     * @param activity the activity details
+     * @return the amount of credits earned
+     */
+    public double calculateCredits(User user, Activity activity) {
+        if (strategy == null) {
+            setStrategy(user);
+        }
+        return strategy.calculateCredits(user, activity);
+    }
+    
+    /**
+     * Calculates debits for a user's equipment usage.
+     *
+     * @param user the user using the equipment
+     * @param usage the usage details
+     * @return the amount of debits incurred
+     */
+    public double calculateDebits(User user, Usage usage) {
+        if (strategy == null) {
+            setStrategy(user);
+        }
+        return strategy.calculateDebits(user, usage);
+    }
 }
--- src/main/java/Strategy/CreditService.java
+++ src/main/java/Strategy/CreditService.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package CreditCalculator;
+package Strategy;
 
 /**
  *
  * @author oenca
  */
-public class java {
+public class CreditService {
     
 }
